43468315015766a1810d7ec84d82e61ec98a947c
Sendy Halim <sendyhalim93@gmail.com>
2016-12-30 16:29:03 +0700

    Install unit test frameworks

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ let package = Package(
   targets: [],
   dependencies: [
     .Package(url: "https://github.com/Alamofire/Alamofire", majorVersion: 4),
-    .Package(url: "https://github.com/kylef/Commander", majorVersion: 0)
+    .Package(url: "https://github.com/kylef/Commander", majorVersion: 0),
+    .Package(url: "https://github.com/Quick/Nimble", majorVersion: 5),
+    .Package(url: "https://github.com/Quick/Quick", majorVersion: 1)
   ]
 );
 
